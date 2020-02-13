3d33a8649b0f8618090e87a3df206e88cf9436d3
Sören Peterson <papa@deep2chill.de>
2020-02-13 01:10:22 -0500

    Projected started, Server Running

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.listen(4000)
+app.listen(4000, err => {
+  if (!err) {
+    console.log("Server Running");
+  } else {
+    console.log(err);
+  }
+});
