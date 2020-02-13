6cd65f0cf5b2a4b0b28756ec8ab1fe92bce9bed5
Sören Peterson <papa@deep2chill.de>
2020-02-13 01:44:12 -0500

    Added Items Model

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   "mongodb://localhost:27017/todo-list",
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   err => {
     console.log(err ? err : "Connected to Database");
   }
